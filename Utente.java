ca0e4c4aa6a1ad6a9ded9b01976301704e79578d
Lorenzo Pergamo <periclepergamo99@gmail.com>
2024-11-20 16:51:05 +0100

    creato costruttore

--- Utente.java
+++ Utente.java
@@ -3,12 +3,20 @@ public class Utente {
     public int punteggio;
     public String passwd; //?
 
+    
+
+    public Utente(String nome, int punteggio, String passwd) {
+        this.nome = nome;
+        this.punteggio = punteggio;
+        this.passwd = passwd;
+    }
+
     public void Stampa(){
         //....
     }
 
     public void modifica(){
-        
+
     }
     
 }
