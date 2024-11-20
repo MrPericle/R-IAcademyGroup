aa2c4ead5cc34915635b434ce737572812c61140
MarcoSantoBarbasso <marcosanto.barbasso@gmail.com>
2024-11-20 17:28:39 +0100

    ho creato l'arrayList

--- RegistroUtenti.java
+++ RegistroUtenti.java
@@ -1,3 +1,20 @@
+import java.util.ArrayList;
+
 public class RegistroUtenti {
-    
+
+    ArrayList<Utente> utenti;
+    public RegistroUtenti() {
+       this.utenti= new ArrayList<Utente>();
+
+    }
+
+
+
+    public void registrazione(Utente ut){
+       
+        utenti.add(ut);
+    }
+
+
+
 }
