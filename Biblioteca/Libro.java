056b42aa65d0d0a89a14b9b9d890f4210f8ec709
Lorenzo Pergamo <periclepergamo99@gmail.com>
2024-11-22 16:00:22 +0100

    Upload Libro

--- Biblioteca/Libro.java
+++ Biblioteca/Libro.java
@@ -1,5 +1,36 @@
 package Biblioteca;
 
 public class Libro {
+    private int numeroCopie;
+    private String nome;
+    private String anno;
 
+    
+
+    public Libro(int numeroCopie, String nome, String anno) {
+        this.numeroCopie = numeroCopie;
+        this.nome = nome;
+        this.anno = anno;
+    }
+    
+    public int getNumeroCopie() {
+        return numeroCopie;
+    }
+    public void setNumeroCopie(int numeroCopie) {
+        this.numeroCopie = numeroCopie;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getAnno() {
+        return anno;
+    }
+    public void setAnno(String anno) {
+        this.anno = anno;
+    }
+
+    
 }
