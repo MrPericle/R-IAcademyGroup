e38067b5d4dcccdf5c07f59598f1203d43d5b70d
tsarib <tania.sari.bonaventura@gmail.com>
2024-11-22 16:14:31 +0100

    aggiunta metodo rimuoviLibro

--- Biblioteca/Biblioteca.java
+++ Biblioteca/Biblioteca.java
@@ -7,31 +7,37 @@ public class Biblioteca {
     ArrayList<Libro> libridisponibili = new ArrayList<Libro>();
     ArrayList<Libro> librinoleggiati = new ArrayList<Libro>();
 
-    //aggiungi ibro
-    public void aggiungiLibro(Libro libro){
+    // aggiungi ibro
+    public void aggiungiLibro(Libro libro) {
 
     }
 
-    //rimuovi libro
-    public void rimuoviLibro(Libro libro){
+    // rimuovi libro
+    public void rimuoviLibro(Libro libro) {
+        if (libridisponibili.isEmpty()) {
+            System.out.println("Nessun libro è presente nella biblioteca!");
+        } else {
+            if (libridisponibili.contains(libro)) {
+                libridisponibili.remove(libridisponibili.indexOf(libro));
+                System.out.println("Hai rimosso il libro " + libro.toString() + " dalla biblioteca!");
+            }
+        }
 
     }
 
-    //presta libro
-    public void prestaLibro(Libro libro, Utente utente){
-
-
-    }
-
-    //restituisci libro
-    public void restituisciLibro(Libro libro, Utente utente){
+    // presta libro
+    public void prestaLibro(Libro libro, Utente utente) {
 
     }
 
-    //stampaLibro
-    public void stampaLibriDisponibili(){
-        
+    // restituisci libro
+    public void restituisciLibro(Libro libro, Utente utente) {
+
     }
 
+    // stampaLibro
+    public void stampaLibriDisponibili() {
+
+    }
 
 }
