05a5b6efa8ea84b796fc11d46c5d5ee678f78474
elisadebellis <elisadebellis17@gmail.com>
2024-11-22 16:05:06 +0100

    biblioteca

--- Biblioteca/Biblioteca.java
+++ Biblioteca/Biblioteca.java
@@ -1,5 +1,37 @@
 package Biblioteca;
 
+import java.util.ArrayList;
+
 public class Biblioteca {
+
+    ArrayList<Libro> libridisponibili = new ArrayList<Libro>();
+    ArrayList<Libro> librinoleggiati = new ArrayList<Libro>();
+
+    //aggiungi ibro
+    public void aggiungiLibro(Libro libro){
+
+    }
+
+    //rimuovi libro
+    public void rimuoviLibro(Libro libro){
+
+    }
+
+    //presta libro
+    public void prestaLibro(Libro libro, Utente utente){
+
+
+    }
+
+    //restituisci libro
+    public void restituisciLibro(Libro libro, Utente utente){
+
+    }
+
+    //stampaLibro
+    public void stampaLibriDisponibili(){
+        
+    }
+
 
 }
