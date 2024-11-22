4adc99dbc8c03a4471cee16359834da0fefb4f4b
Lorenzo Pergamo <periclepergamo99@gmail.com>
2024-11-22 16:05:17 +0100

    Upload Libro

--- Biblioteca/Libro.java
+++ Biblioteca/Libro.java
@@ -4,7 +4,6 @@ public class Libro {
     private int numeroCopie;
     private String nome;
     private String anno;
-
     
 
     public Libro(int numeroCopie, String nome, String anno) {
@@ -12,13 +11,34 @@ public class Libro {
         this.nome = nome;
         this.anno = anno;
     }
-    
+
     public int getNumeroCopie() {
         return numeroCopie;
     }
     public void setNumeroCopie(int numeroCopie) {
         this.numeroCopie = numeroCopie;
     }
+
+    public void incrementaNumeroCopie(int increment){
+        if(increment>=1)
+            this.numeroCopie += increment;
+        else System.out.println("Incompatible increment value");
+    }
+
+    public void incrementaNumeroCopie(){
+        this.numeroCopie++;
+    }
+
+    public void decrementaNumeroCopie(int increment){
+        if(increment < 0)
+            this.numeroCopie -= increment;
+        else System.out.println("Incompatible increment value");
+    }
+
+    public void decrementaNumeroCopie(){
+        this.numeroCopie--;
+    }
+
     public String getNome() {
         return nome;
     }
@@ -30,6 +50,42 @@ public class Libro {
     }
     public void setAnno(String anno) {
         this.anno = anno;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((anno == null) ? 0 : anno.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Libro other = (Libro) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (anno == null) {
+            if (other.anno != null)
+                return false;
+        } else if (!anno.equals(other.anno))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro [nome=" + nome + ", anno=" + anno + "]";
     }
 
     
